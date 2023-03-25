3b88867c99b776c07d92ef877af03882d655d11b
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-03-25 13:07:36 +0400

    spa_version_pre_change

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -19,19 +19,34 @@ export const Skills = () => {
                     <span>{`.css { cascading style sheets }`}</span>
                 </div>
                 <div className={s.skillJS}>
-                    <h3>JS</h3>
+                    <h3>JavaScript</h3>
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png?20141107110902" alt="logo" className={s.skillImg}/>
-                    <span>{`console.log( 'JavaScript' )`}</span>
-                </div>
-                <div className={s.skill}>
-                    <h3>React</h3>
-                    <img src="https://www.svgrepo.com/show/303500/react-1-logo.svg" alt="logo" className={s.skillImg}/>
-                    <span>{`<React/>`}</span>
+                    <span>{`console.log( 'dynamic typing' )`}</span>
                 </div>
                 <div className={s.skill}>
                     <h3>TS</h3>
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/512px-Typescript_logo_2020.svg.png?20221110153201" alt="logo" className={s.skillImg}/>
-                    <span>{`TypeScript: string`}</span>
+                    <span>{`adds JavaScript static typing : string`}</span>
+                </div>
+                <div className={s.skill}>
+                    <h3>StoryBook</h3>
+                    <img src="https://iconape.com/wp-content/files/qa/371510/svg/371510.svg" alt="logo" className={s.skillImgHTML}/>
+                    <span>{`a playground for UI components`}</span>
+                </div>
+                <div className={s.skill}>
+                    <h3>Unit Testing</h3>
+                    <img src="https://res.infoq.com/news/2017/02/Live-Unit-Testing/en/headerimage/GettyImages-530815244.jpg" alt="logo" className={s.skillImg}/>
+                    <span>{`to validate that each unit of the software code performs as expected`}</span>
+                </div>
+                <div className={s.skill}>
+                    <h3>React</h3>
+                    <img src="https://www.svgrepo.com/show/303500/react-1-logo.svg" alt="logo" className={s.skillImg}/>
+                    <span>{`<ForBuildingInteractiveUserInterfaces />`}</span>
+                </div>
+                <div className={s.skill}>
+                    <h3>Redux</h3>
+                    <img src="https://cdn.worldvectorlogo.com/logos/redux.svg" alt="logo" className={s.skillImgHTML}/>
+                    <span>{`State management`}</span>
                 </div>
             </div>
         </div>
