8098224f025609435af74484f4e27e7029a2239f
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-05-07 22:00:42 +0400

    betterUI_start

--- src/App.tsx
+++ src/App.tsx
@@ -4,100 +4,22 @@ import {Home} from "./components/Home";
 import {Skills} from "./components/Skills";
 import {Contacts} from "./components/Contacts";
 import {Projects} from "./components/Projects";
-import { NavHashLink } from 'react-router-hash-link';
+import {NavHashLink} from 'react-router-hash-link';
 import {NavLink, Route, Routes} from "react-router-dom";
-
-type NavLinkBorderType = {
-    home: number
-    skills: number
-    projects: number
-    contacts: number
-}
+import {Layout} from "./components/Layout";
 
 function App() {
-
-    let [navLinkBorder, setNavLinkBorder] = useState<NavLinkBorderType>({
-        home: 1,
-        skills: 0,
-        projects: 0,
-        contacts: 0
-    })
-
-    const onHomeClick = () => {
-        setNavLinkBorder({
-            home: 1,
-            skills: 0,
-            projects: 0,
-            contacts: 0
-        })
-    }
-    const onSkillClick = () => {
-        setNavLinkBorder({
-            home: 0,
-            skills: 1,
-            projects: 0,
-            contacts: 0
-        })
-    }
-    const onProjectClick = () => {
-        setNavLinkBorder({
-            home: 0,
-            skills: 0,
-            projects: 1,
-            contacts: 0
-        })
-    }
-    const onContactClick = () => {
-        setNavLinkBorder({
-            home: 0,
-            skills: 0,
-            projects: 0,
-            contacts: 1
-        })
-    }
-
     return (
-        <div className={s.App}>
-            <header className={s.header}>
-                <div className={s.nav}>
-                    <NavLink to='/home' className={navLinkBorder.home === 1 ? `${s.linkNav} ${s.active}` : s.linkNav} onClick={onHomeClick}
-                    >Home</NavLink>
-                    <NavLink to='/skills' className={navLinkBorder.skills === 1 ? `${s.linkNav} ${s.active}` : s.linkNav} onClick={onSkillClick}
-                    >Skills</NavLink>
-                    <NavLink to='/projects' className={navLinkBorder.projects === 1 ? `${s.linkNav} ${s.active}` : s.linkNav} onClick={onProjectClick}
-                    >Projects</NavLink>
-                    <NavLink to='/contacts' className={navLinkBorder.contacts === 1 ? `${s.linkNav} ${s.active}` : s.linkNav} onClick={onContactClick}
-                    >Contacts</NavLink>
-                </div>
-            </header>
-            <main className={s.main}>
-                <Routes>
-                    <Route path='/' element={<Home/>}/>
-                    <Route path='/home' element={<Home/>}/>
-                    <Route path='/skills' element={<Skills/>}/>
-                    <Route path='/projects' element={<Projects/>}/>
-                    <Route path='/contacts' element={<Contacts/>}/>
-                </Routes>
-            </main>
-            <footer className={s.footer}>
-                <h3 className={s.footerHeader}>add me to your friends</h3>
-                <div className={s.footerIconsContainer}>
-                    <a href='https://www.linkedin.com/in/-ilia-kravtsov' target='_blank' rel="noreferrer">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Facebook_F_icon.svg/512px-Facebook_F_icon.svg.png" alt="facebook logo" className={s.footerLogo}/>
-                    </a>
-                    <a href='https://www.linkedin.com/in/-ilia-kravtsov' target='_blank' rel="noreferrer">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/120px-Instagram_logo_2016.svg.png" alt="facebook logo" className={s.footerLogo}/>
-                    </a>
-                    <a href='https://t.me/Ilia_melbourne' target='_blank' rel="noreferrer">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png?20220101141644" alt="facebook logo" className={s.footerLogo}/>
-                    </a>
-                    <a href='https://www.linkedin.com/in/-ilia-kravtsov' target='_blank' rel="noreferrer">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Linkedin.svg/200px-Linkedin.svg.png" alt="linkedin logo" className={s.footerLogo} />
-                    </a>
-                </div>
-                <span>© All rights reserved</span>
-            </footer>
-        </div>
+        <>
+            <Routes>
+                <Route path={'/'} element={<Layout/>}>
+                    <Route path='home' element={<Home/>}/>
+                    <Route path='skills' element={<Skills/>}/>
+                    <Route path='projects' element={<Projects/>}/>
+                    <Route path='contacts' element={<Contacts/>}/>
+                </Route>
+            </Routes>
+        </>
     );
 }
 
@@ -109,4 +31,32 @@ export default App;
                     <h2 className={s.titleContact}>I consider options for remote work</h2>
                     <NavHashLink smooth to='#contacts' className={s.linkContacts}>Contact me</NavHashLink>
                 </div>
+ */
+
+/*
+<footer className={s.footer}>
+                <h3 className={s.footerHeader}>add me to your friends</h3>
+                <div className={s.footerIconsContainer}>
+                    <a href='https://www.linkedin.com/in/-ilia-kravtsov' target='_blank' rel="noreferrer">
+                        <img
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Facebook_F_icon.svg/512px-Facebook_F_icon.svg.png"
+                            alt="facebook logo" className={s.footerLogo}/>
+                    </a>
+                    <a href='https://www.linkedin.com/in/-ilia-kravtsov' target='_blank' rel="noreferrer">
+                        <img
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/120px-Instagram_logo_2016.svg.png"
+                            alt="facebook logo" className={s.footerLogo}/>
+                    </a>
+                    <a href='https://t.me/Ilia_melbourne' target='_blank' rel="noreferrer">
+                        <img
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png?20220101141644"
+                            alt="facebook logo" className={s.footerLogo}/>
+                    </a>
+                    <a href='https://www.linkedin.com/in/-ilia-kravtsov' target='_blank' rel="noreferrer">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Linkedin.svg/200px-Linkedin.svg.png"
+                             alt="linkedin logo" className={s.footerLogo}/>
+                    </a>
+                </div>
+                <span>© All rights reserved</span>
+            </footer>
  */
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,12 +4,14 @@ import s from './Home.module.css'
 export const Home = () => {
     return (
             <div className={s.container}>
-                <div className={s.textGreeting}>
-                    <span className={s.greeting}>Hey! How’s it going?</span>
-                    <span className={s.name}>I'm ilia kravtsov</span>
-                    <span className={s.profession}>a frontend developer</span>
+                <div className={s.colorBlock}></div>
+                <div className={s.photoWrapper}>
+                    <div className={s.photo}></div>
                 </div>
-                <div className={s.photo}></div>
+                <div className={s.textGreeting}>
+                    <span className={s.name}>I'M ILIA KRAVTSOV</span>
+                    <span className={s.profession}>FRONTEND DEVELOPER</span>
+                </div>
             </div>
     );
 };
