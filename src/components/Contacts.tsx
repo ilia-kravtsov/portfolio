f92b2544dd4f43cf24258e3afd1dd2d478e3b747
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-05-14 21:26:49 +0400

    error_done

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import s from './Contacts.module.scss'
 
 export const Contacts = () => {
+
+    let [name, setName] = useState<string>('')
+    let [phone, setPhone] = useState<string>('')
+    let [email, setEmail] = useState<string>('')
+    let [comment, setComment] = useState<string>('')
+    let [error, setError] = useState<string | null>('')
+
+    const nameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setName(e.currentTarget.value)
+        setError(null)
+    }
+    const phoneChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (Number.isInteger(+e.currentTarget.value)) {
+            setPhone(e.currentTarget.value)
+            setError(null)
+        }
+    }
+    const emailChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.currentTarget.value)
+        setError(null)
+    }
+    const commentChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setComment(e.currentTarget.value)
+        setError(null)
+    }
+
+    const sendClick = () => {
+        if (name.trim() !== '' && phone.trim() !== '' && email.trim() !== '' && comment.trim() !== '') {
+            setName('')
+            setPhone('')
+            setEmail('')
+            setComment('')
+        }
+        if (name.trim() === '') {
+            setError('Fill in all the fields')
+        }
+        if (phone.trim() === '') {
+            setError('Fill in all the fields')
+        }
+        if (email.trim() === '') {
+            setError('Fill in all the fields')
+        }
+        if (comment.trim() === '') {
+            setError('Fill in all the fields')
+        }
+    }
 
     return (
         <div className={s.container} >
@@ -16,22 +62,25 @@ export const Contacts = () => {
             </div>
             <form action="" className={s.form}>
                 <div className={s.inputBox}>
-                    <input type="text" required/>
+                    <input type="text" required value={name} onChange={nameChange}/>
                     <span>First name</span>
                 </div>
                 <div className={s.inputBox}>
-                    <input type="number" required/>
+                    <input type="number" required value={phone} onChange={phoneChange}/>
                     <span>Phone number</span>
                 </div>
                 <div className={s.inputBox}>
-                    <input type="text" required/>
+                    <input type="text" required value={email} onChange={emailChange}/>
                     <span>Email</span>
                 </div>
                 <div className={s.inputBox}>
-                    <textarea className={s.textarea} required></textarea>
+                    <textarea className={s.textarea} required value={comment} onChange={commentChange}></textarea>
                     <span>Enter your comment</span>
                 </div>
-                    <a href="#" className={s.sendButton}>
+                <div className={s.errorContainer}>
+                    <span className={error === 'Fill in all the fields' ? s.errorActive : s.errorDisabled}>{error}</span>
+                </div>
+                    <a href="#" className={s.sendButton} onClick={sendClick}>
                         <span className={s.sendTitle}>Send</span>
                         <div className={s.liquid}></div>
                     </a>
