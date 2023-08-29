c04afaf48e29f2dfe582beccea24ba40b1bdd3e6
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-08-29 21:04:01 +0400

    projects_adaptive_done

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -4,8 +4,8 @@ import sb from "../style/Skills.module.scss";
 import {Fade} from "react-awesome-reveal";
 import emailjs from '@emailjs/browser';
 import {Alert, Snackbar} from "@mui/material";
-import {Tilt} from 'react-tilt'
-import {defaultOptions} from '../features/features'
+import Tilt from 'react-parallax-tilt';
+
 
 export const Contacts: FC = () => {
 
@@ -58,41 +58,41 @@ export const Contacts: FC = () => {
                 </Fade>
 
                 <section className={s.content}>
-                    <section className={s.formContainer}>
+                    <section className={s.doubleContainer}>
                         <form action="" className={s.form} ref={form} onSubmit={sendEmail}>
-                            <div className={s.inputBox}>
-                                <input required
-                                       name="user_name"
-                                       className={s.input}/>
-                                <span>First name</span>
-                            </div>
-                            <div className={s.inputBox}>
-                                <input type="number"
-                                       required
-                                       name="phone_number"
-                                       className={s.input}/>
-                                <span>Phone number</span>
-                            </div>
-                            <div className={s.inputBox}>
-                                <input type="email"
-                                       required
-                                       name="user_email"
-                                       className={s.input}/>
-                                <span>Email</span>
-                            </div>
-                            <div className={s.inputBox}>
+                                <div className={s.inputBox}>
+                                    <input required
+                                           name="user_name"
+                                           className={s.input}/>
+                                    <span>First name</span>
+                                </div>
+                                <div className={s.inputBox}>
+                                    <input type="number"
+                                           required
+                                           name="phone_number"
+                                           className={s.input}/>
+                                    <span>Phone number</span>
+                                </div>
+                                <div className={s.inputBox}>
+                                    <input type="email"
+                                           required
+                                           name="user_email"
+                                           className={s.input}/>
+                                    <span>Email</span>
+                                </div>
+                                <div className={s.inputBox}>
                                 <textarea name="message"
                                           className={s.textareaContact}
                                           required>
                                 </textarea>
-                                <span>Enter your comment</span>
-                            </div>
-                            <Tilt options={defaultOptions}>
-                                <button type={'submit'} disabled={waiting}>
-                                    <span>Send</span>
-                                    <div className={s.liquid}></div>
-                                </button>
-                            </Tilt>
+                                    <span>Enter your comment</span>
+                                </div>
+                            <Tilt tiltAngleYInitial={-15}>
+                                    <button type={'submit'} disabled={waiting}>
+                                        <span>Send</span>
+                                        <div className={s.liquid}></div>
+                                    </button>
+                                </Tilt>
                         </form>
                         {<Snackbar open={success} autoHideDuration={6000} onClose={setSuccessClose}>
                             <Alert onClose={setSuccessClose} severity="success">
@@ -109,50 +109,53 @@ export const Contacts: FC = () => {
                                 Just a moment, we're sending your data!
                             </Alert>
                         </Snackbar>}
+
                         <section className={s.links}>
-                            <div className={s.socialLinksContainer}>
-                                <h2>Social links:</h2>
-                                <div className={s.linksContainer}>
-                                    <a target={'_blank'} href={'https://www.linkedin.com/in/-ilia-kravtsov/'}>
-                                        <img
-                                            src="https://ouch-cdn2.icons8.com/eHIHzmHX02221t-bFZtZRHJmb6S4UUBirfHjBFMp_oo/rs:fit:368:368/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvMjQ3/L2NmOThkOWJmLWRl/NTktNDMzYi1hNjQ1/LTUzYjNkMTczMTU4/My5wbmc.png"
-                                            alt="LinkedIn_logo"/>
-                                    </a>
-                                    <a target={'_blank'}
-                                       href={'https://www.facebook.com/profile.php?id=100094314852191'}>
-                                        <img
-                                            src="https://img.icons8.com/?size=2x&id=jZ0kw76QEzJU&format=png"
-                                            alt="facebook_logo"/>
-                                    </a>
-                                    <a target={'_blank'} href={'https://www.instagram.com/kravtsov_ilia_/'}>
-                                        <img
-                                            src="https://ouch-cdn2.icons8.com/9LkOctqjfET0aNP8knLWqXpV9SvLetwEQRnNdq0dnLM/rs:fit:368:368/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvOTI0/L2NjYjgwMjlkLWZh/NWMtNDU5Yy05YjBk/LWI0Yzg2MzI3Zjc0/Mi5wbmc.png"
-                                            alt="inst_logo"/>
-                                    </a>
+                            <Tilt tiltAngleYInitial={10}>
+                                <div className={s.socialLinksContainer}>
+                                    <h2>Social links:</h2>
+                                    <div className={s.linksContainer}>
+                                        <a target={'_blank'} href={'https://www.linkedin.com/in/-ilia-kravtsov/'}>
+                                            <img
+                                                src="https://ouch-cdn2.icons8.com/eHIHzmHX02221t-bFZtZRHJmb6S4UUBirfHjBFMp_oo/rs:fit:368:368/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvMjQ3/L2NmOThkOWJmLWRl/NTktNDMzYi1hNjQ1/LTUzYjNkMTczMTU4/My5wbmc.png"
+                                                alt="LinkedIn_logo"/>
+                                        </a>
+                                        <a target={'_blank'}
+                                           href={'https://www.facebook.com/profile.php?id=100094314852191'}>
+                                            <img
+                                                src="https://img.icons8.com/?size=2x&id=jZ0kw76QEzJU&format=png"
+                                                alt="facebook_logo"/>
+                                        </a>
+                                        <a target={'_blank'} href={'https://www.instagram.com/kravtsov_ilia_/'}>
+                                            <img
+                                                src="https://ouch-cdn2.icons8.com/9LkOctqjfET0aNP8knLWqXpV9SvLetwEQRnNdq0dnLM/rs:fit:368:368/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvOTI0/L2NjYjgwMjlkLWZh/NWMtNDU5Yy05YjBk/LWI0Yzg2MzI3Zjc0/Mi5wbmc.png"
+                                                alt="inst_logo"/>
+                                        </a>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className={s.socialLinksContainer}>
-                                <h2>Quick connect:</h2>
-                                <div className={s.linksContainer}>
-                                    <a target={'_blank'}
-                                       href={'https://e.mail.ru/cgi-bin/sentmsg?To=kravtsov.i.s@mail.ru&from=otvet&afterReload=1'}>
-                                        <img
-                                            src="https://ouch-cdn2.icons8.com/OW9Zh9PhpH8i_nA909trE4eNHqaLDghrdJWihUXQn9Y/rs:fit:368:368/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvOTYv/MzE3NWFhMzAtMmQw/Yi00MDgyLTlhZWMt/ZWUyZGNlYzQwYmM0/LnBuZw.png"
-                                            alt="email_logo"/>
-                                    </a>
-                                    <a target={'_blank'}
-                                       href={'https://api.whatsapp.com/send/?phone=%2B79991800090&text&type=phone_number&app_absent=0'}>
-                                        <img
-                                            src="https://ouch-cdn2.icons8.com/VS5LETMx6xrS16SIpmiNam1f8eRbbZ_0mtsjfnaW90Y/rs:fit:368:368/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvOTY0/L2U0NTdjYWFlLWMy/MWUtNDU5Yi1iMzcy/LTQ4OWIwM2U5ZDgw/OC5wbmc.png"
-                                            alt="whatsApp_logo"/>
-                                    </a>
-                                    <a target={'_blank'} href={'https://t.me/kravtsov_ilia'}>
-                                        <img
-                                            src="https://ouch-cdn2.icons8.com/kSIAJQ2detV1XffgFuJ10fUEsh4Gm2atdnFRLFz3ORI/rs:fit:368:368/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvOTg1/L2I5MDg2ZjUyLTRi/NWMtNDYyZC1iMTli/LTk5Y2Q0NGZiYjI4/Yi5wbmc.png"
-                                            alt="telegram_logo"/>
-                                    </a>
+                                <div className={s.socialLinksContainer}>
+                                    <h2>Quick connect:</h2>
+                                    <div className={s.linksContainer}>
+                                        <a target={'_blank'}
+                                           href={'https://e.mail.ru/cgi-bin/sentmsg?To=kravtsov.i.s@mail.ru&from=otvet&afterReload=1'}>
+                                            <img
+                                                src="https://ouch-cdn2.icons8.com/OW9Zh9PhpH8i_nA909trE4eNHqaLDghrdJWihUXQn9Y/rs:fit:368:368/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvOTYv/MzE3NWFhMzAtMmQw/Yi00MDgyLTlhZWMt/ZWUyZGNlYzQwYmM0/LnBuZw.png"
+                                                alt="email_logo"/>
+                                        </a>
+                                        <a target={'_blank'}
+                                           href={'https://api.whatsapp.com/send/?phone=%2B79991800090&text&type=phone_number&app_absent=0'}>
+                                            <img
+                                                src="https://ouch-cdn2.icons8.com/VS5LETMx6xrS16SIpmiNam1f8eRbbZ_0mtsjfnaW90Y/rs:fit:368:368/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvOTY0/L2U0NTdjYWFlLWMy/MWUtNDU5Yi1iMzcy/LTQ4OWIwM2U5ZDgw/OC5wbmc.png"
+                                                alt="whatsApp_logo"/>
+                                        </a>
+                                        <a target={'_blank'} href={'https://t.me/kravtsov_ilia'}>
+                                            <img
+                                                src="https://ouch-cdn2.icons8.com/kSIAJQ2detV1XffgFuJ10fUEsh4Gm2atdnFRLFz3ORI/rs:fit:368:368/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvOTg1/L2I5MDg2ZjUyLTRi/NWMtNDYyZC1iMTli/LTk5Y2Q0NGZiYjI4/Yi5wbmc.png"
+                                                alt="telegram_logo"/>
+                                        </a>
+                                    </div>
                                 </div>
-                            </div>
+                            </Tilt>
                         </section>
 
                     </section>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,8 +2,8 @@ import React, {FC} from 'react';
 import s from '../style/Projects.module.scss'
 import sb from "../style/About.module.scss";
 import {Fade} from "react-awesome-reveal";
-import {Tilt} from 'react-tilt'
-import {defaultOptions} from '../features/features'
+import Tilt from 'react-parallax-tilt';
+import {Title} from "../components/Title";
 
 export const Projects: FC = () => {
 
@@ -11,39 +11,34 @@ export const Projects: FC = () => {
         <div className={sb.subContainer} id={'projects'}>
             <div className={s.container}>
                 <Fade direction={'up'}>
-
-                    <div className={s.titleContainer}>
-                        <div className={s.dotContainer_left}>
-                            <div className={s.leftDot}></div>
-                        </div>
-                        <h1 className={s.title}>Project<span className={s.me}>$</span></h1>
-                        <div className={s.dotContainer_right}>
-                            <div className={s.rightDot}></div>
-                        </div>
-                    </div>
-
+                    <Title title={'project'}
+                           accentTitle={'$'}
+                           titleBack={''}
+                    />
                 </Fade>
 
                 <div className={s.projectsContainer}>
                     <Fade direction={'up'}>
                         <div className={s.projectContainer}>
                             <div className={s.projectDescription}>
-                                <p>
-                                    In this project, I incorporated some legacy React code,
-                                    utilized older libraries, employed syntax, class components
-                                    that are no longer in mainstream use,
-                                    implemented RESTful features such as registration, loginization, and pagination,
-                                    and developed components using React and Redux.
-                                </p>
+                                <Tilt tiltAngleYInitial={-45} tiltAxis={'y'}>
+                                    <p>
+                                        In this project, I incorporated some legacy React code,
+                                        utilized older libraries, employed syntax, class components
+                                        that are no longer in mainstream use,
+                                        implemented RESTful features such as registration, loginization, and pagination,
+                                        and developed components using React and Redux.
+                                    </p>
+                                </Tilt>
                             </div>
-                            <Tilt options={defaultOptions}>
+                            <Tilt tiltAngleYInitial={15}>
                                 <div className={s.project}>
                                     <a href='https://ilia-kravtsov.github.io/samurai_way/' target='_blank'
                                        className={s.projectLink} rel="noreferrer">
                                         <div className={s.socialNetworkBack}></div>
 
                                     </a>
-                                    <h3 className={s.header}>Social network</h3>
+                                    <h3 className={s.projectHeader}>Social network</h3>
                                     <span
                                         className={s.status}>Status: in progress
                                     </span>
@@ -52,37 +47,41 @@ export const Projects: FC = () => {
                         </div>
 
                         <div className={s.projectContainer}>
-                            <Tilt options={defaultOptions}>
+                            <Tilt tiltAngleYInitial={-15}>
                                 <div className={s.project}>
                                     <a href='https://ilia-kravtsov.github.io/todolist/' target='_blank'
                                        className={s.projectLink} rel="noreferrer">
                                         <div className={s.todolistBack}></div>
                                     </a>
-                                    <h3 className={s.header}>Todolist</h3>
+                                    <h3 className={s.projectHeader}>Todolist</h3>
                                     <span
                                         className={s.status}>Status: in progress
                                 </span>
                                 </div>
                             </Tilt>
                             <div className={s.projectDescription}>
-                                <p>
-                                    Here i used modern syntax like: Redux-Toolkit,
-                                    Project architectural solutions
-                                    (separation into layers: UI, BLL, DAL, Server)
-                                    Using BLL state management with Redux, Thunks
-                                </p>
+                                <Tilt tiltAngleYInitial={45} tiltAxis={'y'}>
+                                    <p>
+                                        Here i used modern syntax like: Redux-Toolkit,
+                                        Project architectural solutions
+                                        (separation into layers: UI, BLL, DAL, Server)
+                                        Using BLL state management with Redux, Thunks
+                                    </p>
+                                </Tilt>
                             </div>
                         </div>
 
                         <div className={s.projectContainer}>
                             <div className={s.projectDescription}>
-                                <p>
-                                    I've made an effort to showcase
-                                    my passion for mobile app development it's
-                                    an area that I also gravitate towards
-                                </p>
+                                <Tilt tiltAngleYInitial={-45} tiltAxis={'y'}>
+                                    <p>
+                                        I've made an effort to showcase
+                                        my passion for mobile app development it's
+                                        an area that I also gravitate towards
+                                    </p>
+                                </Tilt>
                             </div>
-                            <Tilt options={defaultOptions}>
+                            <Tilt tiltAngleYInitial={15}>
                                 <div className={s.project}>
 
                                     <a href='https://ilia-kravtsov.github.io/telegram_clone_2' target='_blank'
@@ -90,7 +89,7 @@ export const Projects: FC = () => {
                                         <div className={s.telegram}></div>
                                     </a>
 
-                                    <h3 className={s.header}>Telegram clone</h3>
+                                    <h3 className={s.projectHeader}>Telegram clone</h3>
                                     <span
                                         className={s.status}>Status: in progress
                                 </span>
@@ -99,25 +98,27 @@ export const Projects: FC = () => {
                         </div>
 
                         <div className={s.projectContainer}>
-                            <Tilt options={defaultOptions}>
+                            <Tilt tiltAngleYInitial={-15}>
                                 <div className={s.project}>
                                     <a href='https://ilia-kravtsov.github.io/counter/' target='_blank'
                                        className={s.projectLink}
                                        rel="noreferrer">
                                         <div className={s.counter}></div>
                                     </a>
-                                    <h3 className={s.header}>Counter</h3>
+                                    <h3 className={s.projectHeader}>Counter</h3>
                                     <span
                                         className={s.status}>Status: my first project
                                 </span>
                                 </div>
                             </Tilt>
                             <div className={s.projectDescription}>
-                                <p>
-                                    This is my first react project,
-                                    and what makes it truly delightful
-                                    is its simplicity and conciseness.
-                                </p>
+                                <Tilt tiltAngleYInitial={45} tiltAxis={'y'}>
+                                    <p>
+                                        This is my first react project,
+                                        and what makes it truly delightful
+                                        is its simplicity and conciseness.
+                                    </p>
+                                </Tilt>
                             </div>
                         </div>
 
